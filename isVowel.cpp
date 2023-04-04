c0fba83b25dead5d71d3e75067baef0e392bfc84
varunnaik1121 <varunnaik1121@gmail.com>
2023-04-04 16:36:31 +0530

    second commit

---
+++ isVowel.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char letter;
+
+    cout << "enter a upperCase to check vowel or consonent ";
+    cin >> letter;
+    char upperCase;
+    upperCase = tolower(letter);
+    if (upperCase == 'a' || upperCase == 'e' || upperCase == 'i' || upperCase == 'o' || upperCase == 'u')
+    {
+        cout << upperCase << " is "
+             << "vowel";
+    }
+    else
+    {
+        cout << upperCase << " is "
+             << "consonent";
+    }
+}
