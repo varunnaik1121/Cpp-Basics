80b0409a6037782f1f348e85caa2da221636e467
varunnaik1121 <varunnaik1121@gmail.com>
2023-04-08 23:47:09 +0530

    first commit after 6 pm

--- patterns/downwardTriangle.cpp
+++ patterns/downwardTriangle.cpp
@@ -6,13 +6,13 @@ int main()
     cout << "enter the number of rows ";
     cin >> n;
     // to print the number of rows
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= i; j++) // i=0
+        for (int j = 0; j < i; j++)
         {
             cout << " ";
         }
-        for (int j = i; j <= n; j++)
+        for (int j = i; j < n; j++)
         {
             cout << "* ";
         }
