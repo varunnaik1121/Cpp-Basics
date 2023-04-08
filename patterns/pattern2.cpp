158677c886e8e0f84e6fe7e63d774a520614aeb1
varunnaik1121 <varunnaik1121@gmail.com>
2023-04-08 16:28:01 +0530

    first commit

---
+++ patterns/pattern2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number of rows ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
