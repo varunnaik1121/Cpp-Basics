dd22ef8231366c8338752ac90b2d1182e6136a62
varunnaik1121 <varunnaik1121@gmail.com>
2023-04-13 01:50:54 +0530

    first commit

---
+++ patterns/halfButterFly.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the number of rows ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = i; j <= n; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = i; j <= n; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+    // for (int i = 1; i < n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int j = i; j < n; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    return 0;
+}
