1399ab44d0c0e5c3eb2a7ad6c019cefa07cfe506
varunnaik1121 <varunnaik1121@gmail.com>
2023-07-04 12:30:07 +0530

    Day-2 second commit

--- NEW-DSA/Arrays/findDuplicate.cpp
+++ NEW-DSA/Arrays/findDuplicate.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
 using namespace std;
+int findDuplicate(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            return arr[i];
+        }
+    }
+    return -1;
+}
 int main()
 {
     // finding the duplicate among the arrays given by n where n-1 are numbers//[1,2,3,4,4]=>2,3,4 if we XOR with 1 2 3 4 ans will 1wbe the unique one
     int n = 5;
     int arr[] = {
-        1,
+
         1,
         2,
         3,
         4,
+        4
 
     };
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-    {
-        ans ^= arr[i];
-    }
-    for (int i = 1; i < n; i++)
-    {
-        ans ^= i;
-    }
-    cout << ans << endl;
+    // int ans = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     ans ^= arr[i];
+    // }
+    // for (int i = 1; i < n; i++)
+    // {
+    //     ans ^= i;
+    // }
+    // cout << ans << endl;
+
+    cout << findDuplicate(arr, 5);
 
     return 0;
 }
