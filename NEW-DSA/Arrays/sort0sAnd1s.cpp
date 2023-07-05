c1b988ce5672a1caf1e7f81e545e4cd149026e32
varunnaik1121 <varunnaik1121@gmail.com>
2023-07-05 19:00:05 +0530

    Day-3 second commit

--- NEW-DSA/Arrays/sort0sAnd1s.cpp
+++ NEW-DSA/Arrays/sort0sAnd1s.cpp
@@ -4,35 +4,75 @@ int main()
 {
     // basic approach with O(N) time complexity
     // can be sorted so all the zeros are will be on left side
-    // two pointer with same array swap
-    int arr[] = {0, 1, 1, 0, 1, 1, 0, 1, 1, 0};
+    // two pointer with same array swap 
+    //Time complexity is O(N)
+
+    int arr[] = {
+        0,
+        1,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+    };
     int sorted[10];
-    int n = 10;
+    int n = 14;
     int i = 0;
     int j = n - 1;
 
-    // for (int start = 0; start < n; start++)
-    // {
-    //     if (arr[start] == 0)
-    //     {
-    //         sorted[i] = arr[start];
-    //         i++;
-    //     }
-    //     else
-    //     {
-    //         sorted[j] = arr[start];
-    //         j--;
-    //     }
-    // }
-
-    // for (int i = 0; i < ; i++)
-    // {
-    //     cout << sorted[i];
-    // }
-
     // two pointer with same array swap
 
-   for()
+    while ((i <= j))
+    {
+        if (arr[i] == 0)
+        {
+            i++;
+        }
+        if (arr[j] == 1)
+        {
+            j--;
+        }
+        else if (arr[i] == 1 && arr[j] == 0)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
 
     return 0;
 }
+
+// for (int start = 0; start < n; start++)
+// {
+//     if (arr[start] == 0)
+//     {
+//         sorted[i] = arr[start];
+//         i++;
+//     }
+//     else
+//     {
+//         sorted[j] = arr[start];
+//         j--;
+//     }
+// }
+
+// for (int i = 0; i < ; i++)
+// {
+//     cout << sorted[i];
+// }
