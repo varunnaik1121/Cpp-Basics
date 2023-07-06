c504bdaacdfe836eb6f7f6e55f5482c9b76f2502
varunnaik1121 <varunnaik1121@gmail.com>
2023-07-06 22:56:40 +0530

    Day-4 second commit

--- NEW-DSA/BinarySearch/binarySearch.cpp
+++ NEW-DSA/BinarySearch/binarySearch.cpp
@@ -8,7 +8,7 @@ int binarySearch(int arr[], int n, int target)
     end = n - 1;
     while (start <= end)
     {
-        mid = (start + end) / 2;
+        mid = start + (end - start) / 2;
         if (arr[mid] == target)
         {
             flag = 1;
@@ -30,7 +30,7 @@ int binarySearch(int arr[], int n, int target)
 int main()
 {
     int arr[] = {1, 3, 4, 6, 7, 9, 10};
-    int target = 5;
+    int target = 4;
     cout << binarySearch(arr, 7, target);
     return 0;
 }
