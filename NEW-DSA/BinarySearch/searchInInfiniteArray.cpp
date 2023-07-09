1430dd1a5f0fee103c69fee5601afa539c4de28a
varunnaik1121 <varunnaik1121@gmail.com>
2023-07-09 11:26:14 +0530

    Day-6 First Commit

--- NEW-DSA/BinarySearch/searchInInfiniteArray.cpp
+++ NEW-DSA/BinarySearch/searchInInfiniteArray.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int search(int arr[], int target)
+int search(int arr[], int target, int startIndex, int endIndex)
 {
-    int start, end, mid, index = -1;
-    start = 0;
-    end = start + 1;
+    int start = startIndex, end = endIndex, mid;
+
     while (start <= end)
     {
         mid = (start + end) / 2;
@@ -13,13 +12,13 @@ int search(int arr[], int target)
         {
             return mid;
         }
-        else if (target>arr[mid])
+        else if (target > arr[mid])
         {
-            end = mid - 1;
+            start = mid + 1;
         }
         else
         {
-            start = mid + 1;
+            end = mid - 1;
         }
     }
 
@@ -28,8 +27,18 @@ int search(int arr[], int target)
 int main()
 {
 
-    int arr[] = {0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 21, 32, 33, 44};
-    int target = 7;
-    cout << "the ceil of the array  is at index " << search(arr, target);
+    int arr[] = {0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 21, 32, 33, 44, 77, 88, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109}; // target is 8
+    int target = 77;                                                                                                                 // 19
+    int start = 0;
+    int end = 1;
+    while (target > arr[end])
+    {
+        int newStart = end + 1; // value is 2
+        end = end + (end - start + 1) * 2;
+        start = newStart;
+    }
+    cout << "end value is " << end << endl;
+    cout << search(arr, target, start, end) << endl;
+
     return 0;
 }
