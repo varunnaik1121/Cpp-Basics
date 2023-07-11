35cadde5600064fc58e65b1a742d717c3310ef1c
varunnaik1121 <varunnaik1121@gmail.com>
2023-07-11 19:39:17 +0530

    Day-8 First Commit

--- NEW-DSA/BinarySearch/pivotInRotatedArray.cpp
+++ NEW-DSA/BinarySearch/pivotInRotatedArray.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 int findPivot(int arr[], int size)
 {
-    int start, end, mid, index = -1;
+    int start, end, mid;
     start = 0;
     end = size - 1;
     while (start < end)
     {
         mid = (start + end) / 2;
-        if (arr[mid] >= arr[0])
+        if (arr[mid] > arr[0])
         {
-            start = mid + 1;
+            start = mid;
         }
         else
         {
-            mid = end;
+            end = mid - 1;
         }
     }
-    return start;
+    return end;
 }
 int main()
 {
     // basic approach would be linear search and find the smallest Time Complexity will be O(N)
-    int arr[] = {2, 3, 5, 1};
-    int size = 4;
+    int arr[] = {5, 1, 2};
+    int size = 3;
     cout << "the peak element is at index " << findPivot(arr, size);
     return 0;
 }
